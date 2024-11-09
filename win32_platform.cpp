e6083f4db49a25b86c5272d67481f0f87d2ed51d
gmark1337 <149498556+gmark1337@users.noreply.github.com>
2024-11-09 19:24:38 +0100

    Add files via upload

---
+++ win32_platform.cpp
@@ -0,0 +1,116 @@
+#include <Windows.h>
+#include "utils.cpp";
+
+
+global_variable bool running = true;
+
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+#include "platform_common.cpp"
+#include "renderer.cpp"
+#include "game.cpp"
+
+LRESULT CALLBACK window_callback(HWND hWnd, UINT uMsg, WPARAM  wParam, LPARAM  lParam) {
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		}break;
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hWnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE); //freeing existing buffer memory if allocated
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		}break;
+		default: {
+			result = DefWindowProc(hWnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLineint, int nShowCmd) { //Entry point
+
+	//Create Window class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+	//Register class
+	RegisterClass(&window_class);
+
+	//Create Window
+	HWND window = CreateWindowEx(0,window_class.lpszClassName, "My First Game!", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
+
+	Input input = {};
+
+	while (running) {
+		//input
+		MSG message;
+
+		for (int i = 0; i < BUTTON_COUNT; i++) {
+			input.buttons[i].changed = false;
+		}
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+
+			switch (message.message) {
+				case WM_KEYUP:
+				case WM_KEYDOWN: {
+					u32 vk_code = (u32)message.wParam;
+					bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+#define process_button(b, vk)\
+case vk: {\
+input.buttons[b].is_down = is_down;\
+input.buttons[b].changed = true;\
+}break;
+
+					switch (vk_code) {
+						process_button(BUTTON_UP, VK_UP);
+						process_button(BUTTON_DOWN, VK_DOWN);
+						process_button(BUTTON_LEFT, VK_LEFT);
+						process_button(BUTTON_RIGHT, VK_RIGHT);
+
+					}
+				}break;
+				default: {
+
+				}
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			}
+			
+		}
+		//Simulate
+		simulate_game(&input);
+
+
+		//Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+		
+
+	}
+}
